b7135a28cea0de03f2ff14a85ab36019659245c6
Rob Lambell <rob@lambell.info>
2014-02-16 20:54:32 +0000

    Initial commit

---
+++ LittleBin/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LittleBin
+{
+    partial class LittleBin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.vistaMenu1 = new wyDay.Controls.VistaMenu(this.components);
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem6 = new System.Windows.Forms.MenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.vistaMenu1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vistaMenu1
+            // 
+            this.vistaMenu1.ContainerControl = this;
+            // 
+            // menuItem2
+            // 
+            this.vistaMenu1.SetImage(this.menuItem2, global::LittleBin.Properties.Resources.ContextMenuItemIcon);
+            this.menuItem2.Index = 1;
+            this.menuItem2.Text = "Empty Recycle Bin";
+            this.menuItem2.Click += new System.EventHandler(this.OnEmpty);
+            // 
+            // contextMenu1
+            // 
+            this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2,
+            this.menuItem3,
+            this.menuItem4,
+            this.menuItem5,
+            this.menuItem6});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.DefaultItem = true;
+            this.menuItem1.Index = 0;
+            this.menuItem1.Text = "Open";
+            this.menuItem1.Click += new System.EventHandler(this.OnOpen);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 2;
+            this.menuItem3.Text = "-";
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Index = 3;
+            this.menuItem4.Text = "Properties";
+            this.menuItem4.Click += new System.EventHandler(this.OnProperties);
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Index = 4;
+            this.menuItem5.Text = "-";
+            this.menuItem5.Visible = false;
+            // 
+            // menuItem6
+            // 
+            this.menuItem6.Index = 5;
+            this.menuItem6.Text = "Exit";
+            this.menuItem6.Visible = false;
+            this.menuItem6.Click += new System.EventHandler(this.OnExit);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "Recycle Bin";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LittleBin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LittleBin";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vistaMenu1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private wyDay.Controls.VistaMenu vistaMenu1;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem6;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ LittleBin/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LittleBin
+{
+    public partial class LittleBin : Form
+    {
+        public static LittleBin staticVar = null;
+        
+        // SHEmptyRecycleBin
+        // http://msdn.microsoft.com/en-us/library/windows/desktop/bb762160(v=vs.85).aspx
+        enum RecycleFlags : uint
+        {
+            SHERB_NOCONFIRMATION = 0x00000001,
+            SHERB_NOPROGRESSUI = 0x00000002,
+            SHERB_NOSOUND = 0x00000004
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+            static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+
+        // SHObjectProperties
+        public enum ShOP : uint
+        {
+            SHOP_PRINTERNAME = 0x00000001, // lpObject points to a printer friendly name
+            SHOP_FILEPATH = 0x00000002, // lpObject points to a fully qualified path+file name
+            SHOP_VOLUMEGUID = 0x00000004 // lpObject points to a Volume GUID
+        };
+        [DllImport("shell32.dll", EntryPoint = "SHObjectProperties", CharSet = CharSet.Unicode)]
+        public static extern bool SHObjectProperties(IntPtr hwnd, ShOP dwType, string szObject, string szPage);
+
+        // SHQueryRecycleBin
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Pack = 1)]
+        public struct SHQUERYRBINFO
+        {
+            public Int32 cbSize;
+            public UInt64 i64Size;
+            public UInt64 i64NumItems;
+        }
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        public static extern int SHQueryRecycleBin(string pszRootPath, ref SHQUERYRBINFO pSHQueryRBInfo);
+        
+        public LittleBin()
+        {
+            InitializeComponent();
+            notifyIcon1.ContextMenu = contextMenu1;
+        }
+
+        private void OnOpen(object sender, EventArgs e)
+        {
+            Process.Start("shell:RecycleBinFolder");
+        }
+
+        private void OnEmpty(object sender, EventArgs e)
+        {
+            SHEmptyRecycleBin(IntPtr.Zero, null, 0);
+        }
+
+        private void OnProperties(object sender, EventArgs e)
+        {
+            SHObjectProperties(Handle, ShOP.SHOP_FILEPATH, "shell:RecycleBinFolder", null);
+        }
+
+        private void OnExit(object sender, EventArgs e) 
+        { 
+            Application.Exit(); 
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Process.Start("shell:RecycleBinFolder");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            SHQUERYRBINFO query = new SHQUERYRBINFO();
+            query.cbSize = Marshal.SizeOf(typeof(SHQUERYRBINFO));
+
+            try
+            {
+                if (SHQueryRecycleBin(null, ref query) == 0)
+                {
+                    int num_files = (int)query.i64NumItems;
+                    string total_size = (Convert.ToInt64(query.i64Size)).ToFileSize();
+
+                    if (num_files > 0)
+                    {
+                        menuItem2.Enabled = true;
+                        notifyIcon1.Icon = Properties.Resources.FullIcon;
+                        notifyIcon1.Text = "Recycle Bin\n" + total_size + " in " + num_files + (num_files > 1 ? " items" : " item");
+                    }
+                    else
+                    {
+                        menuItem2.Enabled = false;
+                        notifyIcon1.Icon = Properties.Resources.EmptyIcon;
+                        notifyIcon1.Text = "Recycle Bin";
+                    }
+                }
+                else
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.Text = string.Format("Error accessing Recycle Bin: {0}", ex.Message);
+            }
+
+        }
+
+
+    }
+}
